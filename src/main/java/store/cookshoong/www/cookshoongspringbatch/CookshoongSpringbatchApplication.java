b343e6088c4d2088187a3f97a47de0e59f108f9a
윤동현 <eatpaper1429@gmail.com>
2023-07-03 21:51:42 +0900

    feat: 🎸 추가 cookshoong spring batch initial file

---
+++ src/main/java/store/cookshoong/www/cookshoongspringbatch/CookshoongSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package store.cookshoong.www.cookshoongspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookshoongSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookshoongSpringbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/store/cookshoong/www/cookshoongspringbatch/CookshoongSpringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package store.cookshoong.www.cookshoongspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookshoongSpringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
