aa998acabeffc0599b0415894a19270d5df8db73
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-07-31 17:20:09 +0900

    chore: :robot_face:  임시 설정 유레카 안보이도록 잠시 설정해놓음

--- src/main/java/store/cookshoong/www/cookshoongspringbatch/CookshoongSpringbatchApplication.java
+++ src/main/java/store/cookshoong/www/cookshoongspringbatch/CookshoongSpringbatchApplication.java
@@ -1,15 +1,30 @@
 package store.cookshoong.www.cookshoongspringbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+/**
+ * The type Cookshoong springbatch application.
+ */
+//@EnableAutoConfiguration()
+@SpringBootApplication(exclude = {EurekaClientAutoConfiguration.class, EurekaDiscoveryClientConfiguration.class})
+//@EnableDiscoveryClient
+@EnableBatchProcessing // spring batch module infra structure 을 구성.
+@ConfigurationPropertiesScan
 public class CookshoongSpringbatchApplication {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(CookshoongSpringbatchApplication.class, args);
-
     }
 
 }
--- src/test/java/store/cookshoong/www/cookshoongspringbatch/CookshoongSpringbatchApplicationTests.java
+++ src/test/java/store/cookshoong/www/cookshoongspringbatch/CookshoongSpringbatchApplicationTests.java
@@ -1,9 +1,13 @@
 package store.cookshoong.www.cookshoongspringbatch;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {EurekaClientAutoConfiguration.class, EurekaDiscoveryClientConfiguration.class})
 class CookshoongSpringbatchApplicationTests {
 
 	@Test
