c9c8ce2d50f007f9ea9e42fcb11f5aec39a735a4
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-08-04 21:30:58 +0900

    feat: :guitar: 추가 등급 재산정시 DB에 접근하는 Mapper

---
+++ src/main/java/store/cookshoong/www/cookshoongspringbatch/rank/mapper/RankMapper.java
@@ -0,0 +1,17 @@
+package store.cookshoong.www.cookshoongspringbatch.rank.mapper;
+
+import java.util.List;
+import store.cookshoong.www.cookshoongspringbatch.rank.dto.SelectAccountOrderDto;
+import store.cookshoong.www.cookshoongspringbatch.rank.dto.RankDto;
+
+/**
+ * 등급 재산정을 위한 Mapper.
+ *
+ * @author seungyeon
+ * @since 2023.08.01
+ */
+public interface RankMapper {
+    List<RankDto> selectRankCodes();
+    List<SelectAccountOrderDto> selectOrderCntByAccount();
+    void updateRank();
+}
