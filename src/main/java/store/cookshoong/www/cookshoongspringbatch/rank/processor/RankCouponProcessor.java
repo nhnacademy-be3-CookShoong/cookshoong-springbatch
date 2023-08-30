ed48bf27a61db0d0ad19d30ef41c6d3eb7d2826b
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-08-31 07:53:32 +0900

    feat: :guitar: 추가 회원등급을 읽고 등급쿠폰 발급

--- src/main/java/store/cookshoong/www/cookshoongspringbatch/rank/processor/RankCouponProcessor.java
+++ src/main/java/store/cookshoong/www/cookshoongspringbatch/rank/processor/RankCouponProcessor.java
@@ -1,38 +1,64 @@
 package store.cookshoong.www.cookshoongspringbatch.rank.processor;
 
 import java.time.LocalDate;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 import store.cookshoong.www.cookshoongspringbatch.rank.dto.InsertRankCouponDto;
-import store.cookshoong.www.cookshoongspringbatch.rank.dto.SelectAccountOrderDto;
+import store.cookshoong.www.cookshoongspringbatch.rank.dto.RankDto;
 import store.cookshoong.www.cookshoongspringbatch.rank.dto.SelectAccountRankCouponDto;
-import store.cookshoong.www.cookshoongspringbatch.rank.dto.UpdateAccountRankDto;
 
 /**
- * {설명을 작성해주세요}.
+ * 등급쿠폰 발급 프로세서.
  *
  * @author seungyeon
- * @since 2023.08.30
+ * @since 2023.08.20
  */
 @Slf4j
 @StepScope
 @Component
 public class RankCouponProcessor implements ItemProcessor<SelectAccountRankCouponDto, InsertRankCouponDto> {
+
+    private List<RankDto> rankCodes;
+
     @Override
     public InsertRankCouponDto process(SelectAccountRankCouponDto selectAccountRankCouponDto) throws Exception {
-       log.error("============== RankCoupon Processor Start ===============");
+        log.info("============== RankCoupon Processor Start ===============");
+
         InsertRankCouponDto insertRankCouponDto = new InsertRankCouponDto();
         insertRankCouponDto.setAccountId(selectAccountRankCouponDto.getAccountId());
-        insertRankCouponDto.setCouponPolicyId(selectAccountRankCouponDto.getCouponPolicyId());
 
-        LocalDate startDate = LocalDate.now();
-        LocalDate endDate = startDate.plusDays(selectAccountRankCouponDto.getUsagePeriod());
-
-        insertRankCouponDto.setStartDate(startDate);
-        insertRankCouponDto.setEndDate(endDate);
-        log.error("AccountId : {}, CouponPolicyId : {}", insertRankCouponDto.getAccountId(), insertRankCouponDto.getCouponPolicyId());
+        for (int i = 0; i < rankCodes.size(); i++) {
+            if (selectAccountRankCouponDto.getRankCode().equals(rankCodes.get(i).getRankCode())) {
+                insertRankCouponDto.setCouponPolicyId(rankCodes.get(i).getCouponPolicyId());
+                LocalDate startDate = LocalDate.now();
+                LocalDate endDate = startDate.plusDays(rankCodes.get(i).getUsagePeriod());
+                insertRankCouponDto.setStartDate(startDate);
+                insertRankCouponDto.setEndDate(endDate);
+                break;
+            }
+        }
+        log.info("AccountId : {}, CouponPolicyId : {}", insertRankCouponDto.getAccountId(), insertRankCouponDto.getCouponPolicyId());
         return insertRankCouponDto;
+    }
+
+    /**
+     * JobExecutionContext에 저장한 등급 리스트를 가져와서 List로 저장.
+     *
+     * @param stepExecution the step execution
+     */
+    @BeforeStep
+    public void reGet(StepExecution stepExecution) {
+        JobExecution jobExecution = stepExecution.getJobExecution();
+        ExecutionContext jobContext = jobExecution.getExecutionContext();
+        this.rankCodes = (List<RankDto>) jobContext.get("rankCodes");
+
+        log.warn("rankCodes reGet : {}", rankCodes);
     }
 }
