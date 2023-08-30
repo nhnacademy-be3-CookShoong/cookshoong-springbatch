c8a149917f7811100f4346e648739e64528dc95c
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-08-31 07:53:04 +0900

    feat: :guitar: 추가 bulk insert로 인한 메서드 추가

--- src/main/java/store/cookshoong/www/cookshoongspringbatch/rank/mapper/RankMapper.java
+++ src/main/java/store/cookshoong/www/cookshoongspringbatch/rank/mapper/RankMapper.java
@@ -1,8 +1,10 @@
 package store.cookshoong.www.cookshoongspringbatch.rank.mapper;
 
 import java.util.List;
-import store.cookshoong.www.cookshoongspringbatch.rank.dto.SelectAccountOrderDto;
+import store.cookshoong.www.cookshoongspringbatch.rank.dto.InsertRankCouponDto;
 import store.cookshoong.www.cookshoongspringbatch.rank.dto.RankDto;
+import store.cookshoong.www.cookshoongspringbatch.rank.dto.SelectAccountOrderDto;
+import store.cookshoong.www.cookshoongspringbatch.rank.dto.SelectAccountRankCouponDto;
 
 /**
  * 등급 재산정을 위한 Mapper.
@@ -12,7 +14,16 @@ import store.cookshoong.www.cookshoongspringbatch.rank.dto.RankDto;
  */
 public interface RankMapper {
     List<RankDto> selectRankCodes();
+
     List<SelectAccountOrderDto> selectOrderCntByAccount();
+
     void updateRank();
-    void insertRankCoupon();
+
+    void insertRankCoupon(List<InsertRankCouponDto> coupons);
+
+    List<SelectAccountRankCouponDto> selectAccountRankCoupon();
+
+    void disableConstraints();
+
+    void enableConstraints();
 }
